7d1bd8d7d2ae703df8cbd3bf825d763aebde2f85
shamimahmadup11 <shamimahmadup1@gmail.com>
2024-04-29 19:16:09 +0530

    done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,71 +1,11 @@
 
 
-// import "../App.css";
-// const Navbar = () => {
-//   return (
-//     <div>
-//       <nav className="bg-blue-950 h-20 w-screen flex flex-col lg:flex-row lg:justify-between items-center lg:gap-8 px-8">
-//         <div className="flex items-center">
-//           <a href="/">
-//             <img
-//               src="https://flowbite.com/docs/images/logo.svg"
-//               alt=""
-//               className="h-14 w-16"
-//             />
-//           </a>
-//           <h1 className="text-white text-center font-extrabold text-2xl cursor-pointer">
-//             GeekFood
-//           </h1>
-//         </div>
-
-//         <div className="flex-grow lg:flex lg:justify-center lg:gap-12">
-//           <ul className="flex flex-col lg:flex-row justify-center lg:justify-start gap-4 lg:gap-12 lg:text-xl font-semibold">
-//             <li>
-//               <a href="/" className="text-white hover:text-green-600">
-//                 Home
-//               </a>
-//             </li>
-//             <li>
-//               <a href="/" className="text-white hover:text-green-600">
-//                 Quote
-//               </a>
-//             </li>
-//             <li>
-//               <a href="/" className="text-white hover:text-green-600">
-//                 Restaurant
-//               </a>
-//             </li>
-//             <li>
-//               <a href="/" className="text-white hover:text-green-600">
-//                 Foods
-//               </a> 
-//             </li>
-//             <li>
-//               <a href="/" className="text-white hover:text-green-600">
-//                 Contact
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-
-//         <div>
-//           <button className="bg-green-900 h-14 w-28 hover:bg-green-600 text-white rounded-md">
-//             Log in
-//           </button>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 import "../App.css";
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-blue-950 h-20 w-screen flex flex-col lg:flex-row lg:justify-between items-center lg:gap-8 px-8">
+      <nav className="bg-blue-950 min-h-20 h-auto w-screen flex flex-col lg:flex-row lg:justify-between items-center lg:gap-8 px-8">
         <div className="flex items-center">
           <a href="/">
             <img
