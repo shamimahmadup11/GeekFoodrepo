dc1be8056c0c4200ddb7216638bcbd71903e7bff
shamimahmadup11 <shamimahmadup1@gmail.com>
2024-05-02 18:03:27 +0530

    done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="bg-blue-950 min-h-20 h-auto w-screen flex flex-col lg:flex-row lg:justify-between items-center lg:gap-8 px-8">
+      <nav className="bg-blue-950 min-h-18 h-auto w-screen flex flex-col lg:flex-row lg:justify-between items-center lg:gap-8 px-8">
         <div className="flex items-center">
           <a href="/">
             <img
@@ -52,7 +52,7 @@ const Navbar = () => {
         </div>
 
         <div>
-          <button className="bg-green-900 h-14 w-28 hover:bg-green-600 text-white rounded-md text-2xl font-bold ">
+          <button className="bg-green-900 h-10 w-32 hover:bg-green-600 text-white rounded-md text-xl font-bold ">
             Log in
           </button>
         </div>
